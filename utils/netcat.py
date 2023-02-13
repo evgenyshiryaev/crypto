35b82e72055594781fd2895a19157789892466fa
Evgeny Shiryaev <evgeny.shiryaev@devpool.com>
2023-02-13 20:43:07 +0300

    Upgrade nc

--- utils/netcat.py
+++ utils/netcat.py
@@ -1,33 +1,36 @@
-# https://gist.github.com/leonjza/f35a7252babdf77c8421
-
 import socket
- 
+
 class Netcat:
 
+    DEFAULT_LENGTH = 1024
+
     def __init__(self, ip, port):
-        self.buff = ""
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.connect((ip, port))
 
 
-    def read(self, length = 1024):
+    def read(self, length = DEFAULT_LENGTH):
         return self.socket.recv(length)
- 
 
-    def read_until(self, data):
-        while not data in self.buff:
-            self.buff += self.socket.recv(1024)
- 
-        pos = self.buff.find(data)
-        rval = self.buff[:pos + len(data)]
-        self.buff = self.buff[pos + len(data):]
- 
-        return rval
- 
+
+    def readText(self, length = DEFAULT_LENGTH):
+        return self.read(length).decode()
+
+
+    def readTextUntil(self, until):
+        text = ''
+        while not until in text:
+            text += self.readText()
+        return text
+
 
     def write(self, data):
         self.socket.send(data)
     
+
+    def writeText(self, text):
+        self.write(text.encode())
+
 
     def close(self):
         self.socket.close()
