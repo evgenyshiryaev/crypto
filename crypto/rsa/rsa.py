3e104375bcc603477539a577e76fd3f45e917a48
Evgeny Shiryaev <evgeny.shiryaev@devpool.com>
2023-05-11 23:42:04 +0300

    RSA decrypt with CRT

--- crypto/rsa/rsa.py
+++ crypto/rsa/rsa.py
@@ -10,6 +10,7 @@
 # m - plain text (m < n), c - cypher text
 # c = pow(m, e, n)
 # m = pow(c, d, n) = pow(m , e * d, n)
+
 # public key = (e, n)
 # private key = (d, n)
 
@@ -45,23 +46,39 @@ def blocks_to_text(blocks, block_len, text_len):
     return ''.join(text)
 
 
-def encrypt(plain_text, key, block_len):
+def encrypt(m, key, block_len):
     blocks = []
     e, n = key
-    for block in text_to_blocks(plain_text, block_len):
+    for block in text_to_blocks(m, block_len):
         blocks.append(pow(block, e, n))
     return blocks
 
 
-def decrypt(cipher_text, key, block_len, plain_text_len):
+def decrypt(c, key, block_len, plain_text_len):
     blocks = []
     d, n = key
-    for block in cipher_text:
+    for block in c:
         blocks.append(pow(block, d, n))
     return blocks_to_text(blocks, block_len, plain_text_len)
 
 
+def decrypt_crt(c, d, p, q):
+    # precompute for given key
+    dp = d % (p - 1)
+    dq = d % (q - 1)
+    q_inv = gmpy2.invert(q, p)
+
+    mp = pow(c, dp, p)
+    mq = pow(c, dq, q)
+    h = q_inv * (mp - mq) % p
+    return (mq + h * q) % (p * q)
+
+
 def generate_key(bits):
+    return generate_key_with_pq(bits)[:3]
+
+
+def generate_key_with_pq(bits):
     p, q = getPrime(bits), getPrime(bits)
     n = p * q
     f = (p - 1) * (q - 1)
@@ -70,25 +87,22 @@ def generate_key(bits):
         if e < f and gmpy2.gcd(e, f) == 1:
             break
     d = int(gmpy2.invert(e, f))
-    return e, d, n
+    return e, d, n, p, q
 
 
 if __name__ == '__main__':
     _block_len = 10
+    _m = 'nofatebutwhatwemake'
 
-    _plain_text = 'nofatebutwhatwemake'
-    print(_plain_text)
+    _blocks = text_to_blocks(_m, _block_len)
+    assert _m == blocks_to_text(_blocks, _block_len, len(_m))
 
-    _blocks = text_to_blocks(_plain_text, _block_len)
-    print(_blocks)
-    print(blocks_to_text(_blocks, _block_len, len(_plain_text)))
+    _key = generate_key_with_pq(128)
+    _e, _d, _n, _p, _q = _key
 
-    _bits = 32
-    _key = generate_key(_bits)
-    print(_key)
-    _e, _d, _n = _key
+    _c = encrypt(_m, (_e, _n), _block_len)
+    assert _m == decrypt(_c, (_d, _n), _block_len, len(_m))
 
-    _cipher_text = encrypt(_plain_text, (_e, _n), _block_len)
-    print(_cipher_text)
-
-    print(decrypt(_cipher_text, (_d, _n), _block_len, len(_plain_text)))
+    _m = 696969
+    _c = pow(_m, _e, _n)
+    assert _m == decrypt_crt(_c, _d, _p, _q)
