713c8eb407fdaaf8077eaf92fe2e276ba3565dc7
Evgeny Shiryaev <arpmipg@gmail.com>
2024-06-02 22:47:15 +0300

    meet in the middle to knapsack

--- crypto/asym/merkle_hellman_knapsack.py
+++ crypto/asym/merkle_hellman_knapsack.py
@@ -38,8 +38,47 @@ def decrypt(c, priv_key):
     return long_to_bytes(sum([2 ** (len(W) - xi - 1) for xi in x]))
 
 
+def meet_in_the_middle_hack(c, pub_key):
+    bits = len(pub_key)
+
+    bits_half, middle, result = bits // 2, {}, None
+
+    def left(i, m, m_c):
+        if i == bits_half:
+            middle[m_c] = m
+        else:
+            left(i + 1, m << 1, m_c)
+            left(i + 1, (m << 1) | 1, m_c + pub_key[i])
+
+    def right(i, m, m_c):
+        nonlocal result
+        if result:
+            return
+        if i == bits:
+            m_c0 = c - m_c
+            if m_c0 in middle:
+                result = middle[m_c0], m
+        else:
+            right(i + 1, m << 1, m_c)
+            right(i + 1, (m << 1) | 1, m_c + pub_key[i])
+
+    left(0, 1, 0)
+    right(bits_half, 1, 0)
+
+    x = bin(result[0])[3:] + bin(result[1])[3:]
+    m = b''
+    for _i in range(0, len(x), 8):
+        m += long_to_bytes(int(''.join([str(_b) for _b in x[_i: _i + 8]]), 2))
+    return m
+
+
 if __name__ == '__main__':
     _pub_key, _priv_key = generate_key(256)
     _m = b'soo unsecret'
     _c = encrypt(_m, _pub_key)
     assert _m == decrypt(_c, _priv_key)
+
+    _pub_key, _ = generate_key(48)  # not more
+    _m = b'hack01'
+    _c = encrypt(_m, _pub_key)
+    assert _m == meet_in_the_middle_hack(_c, _pub_key)
